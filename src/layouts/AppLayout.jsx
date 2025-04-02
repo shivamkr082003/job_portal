a4836fbe034c8f56c384d2e600ad2d1b3686a5c2
shivamkr082003 <shivam6132@gmail.com>
2025-04-02 04:44:39 -0700

    added authentication in header component

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -12,7 +12,7 @@ const AppLayout = () => {
         <Outlet/>
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
-        Made by Shivam
+      Made with 💗 by Shivam
       </div>
     </div>
   );
