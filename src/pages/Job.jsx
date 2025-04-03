34b949f0f11702a8777b344a3adf292a76cc6f21
shivamkr082003 <shivam6132@gmail.com>
2025-04-03 08:14:07 -0700

    added apiaplication.jsx and job.jsx

--- src/pages/Job.jsx
+++ src/pages/Job.jsx
@@ -1,19 +1,19 @@
 import { useEffect } from "react";
 import { BarLoader } from "react-spinners";
-import MDEditor from "@uiw/react-md-editor"; 
+import MDEditor from "@uiw/react-md-editor";
 import { useParams } from "react-router-dom";
 import { useUser } from "@clerk/clerk-react";
 import { Briefcase, DoorClosed, DoorOpen, MapPinIcon } from "lucide-react";
 
-// import {
-//   Select,
-//   SelectContent,
-//   SelectItem,
-//   SelectTrigger,
-//   SelectValue,
-// } from "@/components/ui/select";
-// import { ApplyJobDrawer } from "@/components/apply-job";
-// import ApplicationCard from "@/components/application-card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { ApplyJobDrawer } from "@/components/apply-job";
+import ApplicationCard from "@/components/application-card";
 
 import useFetch from "@/hooks/use-fetch";
 import { getSingleJob, updateHiringStatus } from "@/api/apiJobs";
