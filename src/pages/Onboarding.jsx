22cc91156646b851c3973da1602f7a4da724c385
shivamkr082003 <shivam6132@gmail.com>
2025-04-07 03:33:19 -0700

    added onboarding

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { BarLoader } from "react-spinners";
 
+
 const Onboarding = () => {
   const { user, isLoaded } = useUser();
   const navigate = useNavigate();
